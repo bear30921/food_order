95438b340ed5a273ed3d129a1703c91bf535856d
bear30921 <bear30921@gmail.com>
2018-02-25 20:49:26 +0800

    新增景點詳細介紹

--- javascript/main.js
+++ javascript/main.js
@@ -1,10 +1,106 @@
 var map;
+var areaLocation = [{
+	area: "台北市",
+	location: {      
+		lat: 25.0369639,
+		lng: 121.5361861
+	}
+},
+{
+	area: "中正區",
+	location: {      
+		lat: 25.0292998,
+		lng: 121.5030736
+	}
+},
+{
+	area: "大同區",
+	location: {      
+		lat: 25.0645017,
+		lng: 121.4958052
+	}
+},
+{
+	area: "中山區",
+	location: {      
+		lat: 25.0685018,
+		lng: 121.5280918
+	}
+},
+{
+	area: "松山區",
+	location: {      
+		lat: 25.0601717,
+		lng: 121.5417977
+	}
+},
+{
+	area: "大安區",
+	location: {      
+		lat: 25.0263064,
+		lng: 121.5263363
+	}
+},
+{
+	area: "萬華區",
+	location: {      
+		lat: 25.0294925,
+		lng: 121.4803742
+	}
+},
+{
+	area: "信義區",
+	location: {      
+		lat: 25.0287132,
+		lng: 121.5548065
+	}
+},
+{
+	area: "士林區",
+	location: {      
+		lat: 25.1347637,
+		lng: 121.462405
+	}
+},
+{
+	area: "北投區",
+	location: {      
+		lat: 25.1531443,
+		lng: 121.4824091
+	}
+},
+{
+	area: "內湖區",
+	location: {      
+		lat: 25.0834976,
+		lng: 121.5553559
+	}
+},
+{
+	area: "南港區",
+	location: {      
+		lat: 25.0383878,
+		lng: 121.5869684
+	}
+},
+{
+	area: "文山區",
+	location: {      
+		lat: 24.9880031,
+		lng: 121.540252
+	}
+}
+];
+
+var areaLat;
+var areaLng;
+
 // 初始化google地圖
 function initMap() {  
 	map = new google.maps.Map(document.getElementById('map'), {    
 		center: {      
-			lat: 25.0369639,
-			      lng: 121.5361861    
+			lat: areaLat || 25.0369639,
+			lng: areaLng ||  121.5361861   
 		},
 		    zoom: 14  
 	});
@@ -22,18 +118,17 @@ function getData() {  
 		place.lng = Number(data[i].longitude);    
 		box.map = map;    
 		box.title = data[i].stitle;    
-		box.position = place;     // console.log(box);
+		box.position = place;    
 		    
 		new google.maps.Marker(box);  
 	}
 }
 
-function renderData(param) {
+function renderData() {
 	var str = ''; 
 	var placesList = document.querySelector('.places-list');  
 	placesList.innerHTML = '';
-	for (var i = 0; i < 10; i++) {
-		console.log('hello');
+	for (var i = 0; i < 50; i++) {
 
 		var notAvailable = '尚未提供';      
 		var placeImg;      
@@ -50,7 +145,7 @@ function renderData(param) {
 		}
 
 		      
-		var place = '<li class="place" data-index="' + i + '"><div class="caption"><img src="' + placeImg + '" alt=""><h3 class="title">' + placeTitle + '</h3><div class="area">' + placeArea + '</div></div><div class="info"><p class="type">類型：' + placeType + '</p><p>地址：' + placeAddress + '</p><p>電話：' + placeTel + '</p><a href="javascript:;" class="btn">景點介紹</a></div></li>';
+		var place = '<li class="place" data-index="' + (i+1) + '"><div class="caption"><img src="' + placeImg + '" alt=""><h3 class="title">' + placeTitle + '</h3><div class="area">' + placeArea + '</div></div><div class="info"><p class="type">類型：' + placeType + '</p><p>電話：' + placeTel + '</p><p>地址：' + placeAddress + '</p><a href="javascript:;" class="btn">景點介紹</a></div></li>';
 
 		      
 		str += place;  
@@ -67,7 +162,6 @@ function filterData(area) {  
 	placesList.innerHTML = '';
 	for (var i = 0; i < 10; i++) {
 		if (area === data[i].address.substr(5, 3)) {
-			console.log('hello');
 
 			var notAvailable = '尚未提供';      
 			var placeImg;      
@@ -83,10 +177,8 @@ function filterData(area) {  
 				placeImg = data[i].file.img[0]['#text'] || '';      
 			}
 
-			      
-			var place = '<li class="place" data-index="' + i + '"><div class="caption"><img src="' + placeImg + '" alt=""><h3 class="title">' + placeTitle + '</h3><div class="area">' + placeArea + '</div></div><div class="info"><p class="type">類型：' + placeType + '</p><p>地址：' + placeAddress + '</p><p>電話：' + placeTel + '</p><a href="javascript:;" class="btn">景點介紹</a></div></li>';
+			var place = '<li class="place" data-index="' + (i+1) + '"><div class="caption"><img src="' + placeImg + '" alt=""><h3 class="title">' + placeTitle + '</h3><div class="area">' + placeArea + '</div></div><div class="info"><p class="type">類型：' + placeType + '</p><p>地址：' + placeAddress + '</p><p>電話：' + placeTel + '</p><a href="javascript:;" class="btn detail">景點介紹</a></div></li>';
 
-			      
 			str += place;  
 		}
 
@@ -94,18 +186,83 @@ function filterData(area) {  
 	placesList.innerHTML = str;
 }
 
-// renderData();
 
 
 var areaList = document.querySelector('.area-list');
-areaList.addEventListener('change', function (e) {  
-	var currentValue = e.target.value;  
-	if (currentValue === "全部") {
-		renderData();
-	} else {
-		filterData(currentValue);
+
+areaList.addEventListener('change', function (e) { 
+	var currentValue = e.target.value;
+	
+	for (var i in areaLocation) {
+		if (currentValue === "台北市") {
+			areaLat = 25.0369639;
+			areaLng = 121.5361861;
+			renderData();
+			initMap();
+		} else if (currentValue === areaLocation[i].area) {
+			areaLat = areaLocation[i].location.lat;
+			areaLng = areaLocation[i].location.lng;
+			filterData(currentValue);
+			initMap();
+		}
 	}
+
 });
+
+// 打開景點介紹
+var viewDetail = document.querySelector('.views');
+viewDetail.addEventListener('click', function (e) {
+	var currentView = e.target;
+	var currentHTML = document.querySelector("html");
+	currentHTML.classList.add("stop-scrolling");
+	if (currentView.tagName === 'A') {
+		document.querySelector('.screen').style.display = 'block';
+		var currentDom = currentView.parentElement.parentElement;
+		for (var i in data) {
+			if (currentDom.dataset.index === data[i].RowNumber) {
+				var notAvailable = '尚未提供'; 
+				var placeImg;      
+				var placeTitle = data[i].stitle || notAvailable; 
+				var placeArea = data[i].address.substr(5, 3) || notAvailable;      
+				var placeType = data[i].CAT2 || notAvailable;   
+				var placeTel = data[i].MEMO_TEL || notAvailable;    
+				var placeAddress = data[i].address || notAvailable;   
+				var placeMrt = data[i].MRT || notAvailable;   
+				var placeTime = data[i].MEMO_TIME || notAvailable;  
+				var placeDetail = data[i].xbody || notAvailable; 
+				if (data[i].file.img.length === undefined) {        
+					placeImg = data[i].file.img['#text'] || '';      
+				} else {        
+					placeImg = data[i].file.img[0]['#text'] || '';      
+				} 
+				document.querySelector('.view-picture').setAttribute("src", placeImg);
+				document.querySelector('.view-title').textContent = placeTitle;
+				document.querySelector('.view-area').textContent = placeArea;
+				document.querySelector('.view-type').textContent = placeType;
+				document.querySelector('.view-tel').textContent = placeTel;
+				document.querySelector('.view-address').textContent = placeAddress;
+				document.querySelector('.view-mrt').textContent = placeMrt;
+				document.querySelector('.view-time').textContent = placeTime;
+				document.querySelector('.view-detail').textContent = placeDetail;
+			}
+			
+		}
+	}
+}, false);
+
+
+// 關閉景點介紹
+var screen = document.querySelector('.screen');
+screen.addEventListener('click', function (e) {
+	var cancelBtn = e.target;
+	var currentHTML = document.querySelector("html");
+	currentHTML.classList.remove("stop-scrolling");
+	if (cancelBtn.getAttribute('class') === 'screen' || cancelBtn.getAttribute('class') === 'cancel') {
+		document.querySelector('.screen').style.display = 'none';
+	}
+})
+
+
 
 
 // var number = 10;
@@ -122,6 +279,5 @@ areaList.addEventListener('change', function (e) {  
 // ul_list.innerHTML = str;
 
 // document.querySelector('.views .container').appendChild(ul_list);
-
 
 window.onload = renderData();
