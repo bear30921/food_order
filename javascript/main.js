33f980757109ac84ab5b0a2e92b95e13ecdab6af
bear30921 <bear30921@gmail.com>
2018-03-23 21:15:55 +0800

    新增購物車

--- javascript/main.js
+++ javascript/main.js
@@ -1,16 +1,35 @@
 var vm = new Vue({
 	el: '#app',
 	data: {
-		foods: foodData
+		foods: foodData,
+		cart: []
 	},
 	methods: {
 		increase: function (index) {
 			var currentFood = this.foods[index];
-			if (currentFood.count === undefined) {
-				Vue.set(currentFood, 'count', 1);
+			if (currentFood.count === undefined || currentFood.count === 0) {
+				this.$set(currentFood, 'count', 1);
+				this.addCart(currentFood);
 			} else {
 				currentFood.count++;
 			}
+		},
+
+		decrease: function (index) {
+			var currentFood = this.foods[index];
+			if (currentFood.count > 0) {
+				currentFood.count--;
+				if (currentFood.count === 0) {
+					this.removeCart(currentFood);
+				}
+			}
+		},
+		addCart: function (goods) {
+			this.cart.push(goods);
+		},
+		removeCart: function (goods) {
+			var currentPosition = this.cart.indexOf(goods);
+			this.cart.splice(currentPosition, 1);
 		}
 	}
 });
